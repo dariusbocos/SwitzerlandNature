386585a8c04e376046db0c5b80f5b1020159d330
dariusbocos <146333886+dariusbocos@users.noreply.github.com>
2023-09-28 16:29:08 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let option = ['One Day Tour','3 Days Tour', '7 Days Tour'];
+if (option = [0]) {
+   console.log('One Day Tour');
+}
+
+else if (option = [1]) {
+    console.log('3 Days Tour');
+}
+
+else if (option = [2]) {
+    console.log('7 Days Tour');
+}
