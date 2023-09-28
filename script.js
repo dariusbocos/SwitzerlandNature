5e9c2d0dcdb10cc9fde5b7e4eec7a052af4ca1f4
dariusbocos <146333886+dariusbocos@users.noreply.github.com>
2023-09-28 16:33:38 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -10,3 +10,32 @@ else if (option = [1]) {
 else if (option = [2]) {
     console.log('7 Days Tour');
 }
+
+const websiteName = "Switzerland Nature";
+const websiteURL = "https://www.switzerlandnature.com";
+const websiteDescription = "Explore the breathtaking natural beauty of Switzerland.";
+
+const destinations = [
+  "Swiss Alps",
+  "Lake Geneva",
+  "Zermatt and the Matterhorn",
+  "Jungfrau Region",
+  "Swiss National Park"
+];
+
+const attractionImages = [
+  "alps.jpg",
+  "lake-geneva.jpg",
+  "matterhorn.jpg",
+  "jungfrau.jpg",
+  "swiss-national-park.jpg"
+];
+
+const attractionDescriptions = [
+  "Experience the majestic Swiss Alps with their snow-capped peaks.",
+  "Visit the stunning shores of Lake Geneva and enjoy its crystal-clear waters.",
+  "Discover the iconic Matterhorn mountain in the charming town of Zermatt.",
+  "Explore the breathtaking Jungfrau region with its picturesque landscapes.",
+  "Immerse yourself in the natural wonders of the Swiss National Park."
+];
+
